c6a20b5b859a4fbc5fe57f6073912cf673bf72d4
Mallemoose <mofr37729@edu.ucl.dk>
2022-04-16 11:30:51 +0200

    consolelogsplashjs

--- JavaScript/malthe.js
+++ JavaScript/malthe.js
@@ -5,3 +5,11 @@ document.addEventListener('DOMContentLoaded', (e)=>{
         splash.classList.add('display-none');
     }, 3000);
 })
+
+function splashlog(x) {
+	let splash1=document.getElementById("splash1");
+	let splash2=document.getElementById("splash2");
+	let splash3=document.getElementById("splash3");
+ }
+	const uger=["splash1", "splash2", "splash3"];
+	console.log(splashes);
